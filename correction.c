9b8e9ac5658e43599d8eefdce4dc4e14712e48b0
Hayatache <schafrothbenj@outlook.com>
2025-05-16 12:53:13 +0200

    MAJ Q4

---
+++ correction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void testSLO1(int16_t valE, float* tbF) {
+
+}
+void main(void) {
+	int valTest;
+	double tbNote[10];
+
+	TestSLO1((int16_t)valTest, tbNote);
+
+}
